7315fcb3cb96b45f69b94d3328add782c93dd822
Philip Woods <elzair@pop-os.localdomain>
2020-01-16 13:48:33 -0500

    Added basic monsters

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,13 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -55,6 +59,28 @@ fn main() {
 
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+    
+    // Populate rooms with monsters
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+
+        let glyph = match rng.roll_dice(1, 2) {
+            1 => rltk::to_cp437('g'),
+            _ => rltk::to_cp437('o')
+        };
+
+        gs.ecs.create_entity()
+            .with(Position{ x, y })
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{ visible_tiles: Vec::new(), range: 8, dirty: true })
+            .build();
+    }
+
     gs.ecs.insert(map);
 
     gs.ecs
