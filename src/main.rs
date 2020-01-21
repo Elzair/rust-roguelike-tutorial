bad60fb3b913b87d864d614dd7dbebe2ffe73e1d
Philip Woods <elzairthesorcerer@gmail.com>
2020-01-21 10:03:53 -0500

    Finished chapter 7

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate specs_derive;
 mod components;
 pub use components::{BlocksTile, CombatStats, Monster, Name, Player, Position, Renderable, SufferDamage, Viewshed, WantsToMelee};
 mod damage_system;
-pub use damage_system::{DamageSystem, delete_the_dead};
+pub use damage_system::DamageSystem;
 mod map;
 pub use map::*;
 mod map_indexing_system;
@@ -25,11 +25,10 @@ mod visibility_system;
 pub use visibility_system::VisibilitySystem;
 
 #[derive(Clone, Copy, PartialEq)]
-pub enum RunState { Paused, Running }
+pub enum RunState { AwaitingInput, MonsterTurn, PreRun, PlayerTurn }
 
 pub struct State {
     pub ecs: World,
-    pub runstate: RunState,
 }
 
 impl State {
@@ -52,13 +51,35 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
-        if self.runstate == RunState::Running {
-            self.run_systems();
-            delete_the_dead(&mut self.ecs);
-            self.runstate = RunState::Paused;
-        } else {
-            self.runstate = player_input(self, ctx);
+        let mut newrunstate;
+        {
+            let runstate = self.ecs.fetch::<RunState>();
+            newrunstate = *runstate;
         }
+
+        match newrunstate {
+            RunState::PreRun => {
+                self.run_systems();
+                newrunstate = RunState::AwaitingInput;
+            }
+            RunState::AwaitingInput => {
+                newrunstate = player_input(self, ctx);
+            }
+            RunState::PlayerTurn => {
+                self.run_systems();
+                newrunstate = RunState::MonsterTurn;
+            }
+            RunState::MonsterTurn => {
+                self.run_systems();
+                newrunstate = RunState::AwaitingInput;
+            }
+        }
+
+        {
+            let mut runwriter = self.ecs.write_resource::<RunState>();
+            *runwriter = newrunstate;
+        }
+        damage_system::delete_the_dead(&mut self.ecs);
 
         draw_map(&self.ecs, ctx);
 
@@ -79,7 +100,6 @@ fn main() {
     let context = Rltk::init_simple8x8(80, 50, "Hello Rust World", "resources");
     let mut gs = State { 
         ecs: World::new(),
-        runstate: RunState::Running,
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
@@ -95,6 +115,7 @@ fn main() {
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(Point::new(player_x, player_y));
+    gs.ecs.insert(RunState::PreRun);
     
     // Populate rooms with monsters
     let mut rng = rltk::RandomNumberGenerator::new();
@@ -123,7 +144,7 @@ fn main() {
 
     gs.ecs.insert(map);
 
-    gs.ecs
+    let player_entity = gs.ecs
         .create_entity()
         .with(Position { x: player_x, y: player_y })
         .with(Renderable {
@@ -136,6 +157,7 @@ fn main() {
         .with(Name{name: "Player".to_string() })
         .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5 })
         .build();
+    gs.ecs.insert(player_entity);
 
     rltk::main_loop(context, gs);
 }
--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,6 +1,7 @@
-use super::components::{Monster, Name, Position, Viewshed};
+use super::RunState;
+use super::components::{Monster, Position, Viewshed, WantsToMelee};
 use super::map::Map;
-use rltk::{console, Point};
+use rltk::Point;
 use specs::prelude::*;
 
 pub struct MonsterAI {}
@@ -10,33 +11,41 @@ impl<'a> System<'a> for MonsterAI {
     type SystemData = (
         WriteExpect<'a, Map>,
         ReadExpect<'a, Point>,
+        ReadExpect<'a, Entity>,
+        ReadExpect<'a, RunState>,
+        Entities<'a>,
         WriteStorage<'a, Viewshed>, 
         ReadStorage<'a, Monster>,
-        ReadStorage<'a, Name>,
         WriteStorage<'a, Position>,
+        WriteStorage<'a, WantsToMelee>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, player_pos, mut viewshed, monster, name, mut position) = data;
+        let (mut map, player_pos, player_entity, runstate, entities, mut viewshed, monster, mut position, mut wants_to_melee) = data;
 
-        for (mut viewshed, _monster, name, mut pos) in (&mut viewshed, &monster, &name, &mut position).join() {
+        if *runstate != RunState::MonsterTurn { return; }
+
+        for (entity, mut viewshed, _monster, mut pos) in (&entities, &mut viewshed, &monster, &mut position).join() {
             let distance = rltk::DistanceAlg::Pythagoras.distance2d(Point::new(pos.x, pos.y), *player_pos);
             if viewshed.visible_tiles.contains(&*player_pos) {
                 if distance < 1.5 {
-                    // Attack goes here.
-                    console::log(format!("{} shouts insults", name.name));
-                    continue; // Should this be a return?
-                }
-
-                let path = rltk::a_star_search(
-                    map.xy_idx(pos.x, pos.y).unwrap() as i32, 
-                    map.xy_idx(player_pos.x, player_pos.y).unwrap() as i32, 
-                    &mut *map
-                );
-                if path.success && path.steps.len()>1 {
-                    pos.x = path.steps[1] % map.width;
-                    pos.y = path.steps[1] / map.width;
-                    viewshed.dirty = true;
+                    wants_to_melee.insert(entity, WantsToMelee{ target: *player_entity }).expect("Unable to insert attack");
+                } else if viewshed.visible_tiles.contains(&*player_pos) {
+                    // Path to the player
+                    let path = rltk::a_star_search(
+                        map.xy_idx(pos.x, pos.y).unwrap() as i32, 
+                        map.xy_idx(player_pos.x, player_pos.y).unwrap() as i32, 
+                        &mut *map
+                    );
+                    if path.success && path.steps.len() > 1 {
+                        let mut idx = map.xy_idx(pos.x, pos.y).unwrap();
+                        map.blocked[idx] = false;
+                        pos.x = path.steps[1] % map.width;
+                        pos.y = path.steps[1] / map.width;
+                        idx = map.xy_idx(pos.x, pos.y).unwrap();
+                        map.blocked[idx] = true;
+                        viewshed.dirty = true;
+                    }
                 }
             }
         }
