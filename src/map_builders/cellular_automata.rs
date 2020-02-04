1092dd49aee980a96ea4d942e16042275674d9da
Philip Woods <elzairthesorcerer@gmail.com>
2020-02-04 11:04:27 -0500

    Finished chapter 27

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -1,20 +1,19 @@
-use rltk::{DijkstraMap, RandomNumberGenerator};
+use rltk::{ CellularDistanceFunction, DijkstraMap, FastNoise, RandomNumberGenerator };
 use specs::prelude::*;
+use std::collections::HashMap;
 
 use super::super::components::Position;
 use super::super::map::{Map, TileType};
-use super::super::rect::Rect;
 use super::super::spawner;
 use super::super::SHOW_MAPGEN_VISUALIZER;
 use super::MapBuilder;
-
-const MIN_ROOM_SIZE: i32 = 8;
 
 pub struct CellularAutomataBuilder {
     map: Map,
     starting_position: Position,
     depth: i32,
     history: Vec<Map>,
+    noise_areas: HashMap<i32, Vec<usize>>,
 }
 
 impl CellularAutomataBuilder {
@@ -24,6 +23,7 @@ impl CellularAutomataBuilder {
             starting_position: Position { x: 0, y: 0 },
             depth: new_depth,
             history: Vec::new(),
+            noise_areas: HashMap::new(),
         }
     }
 
@@ -139,6 +139,27 @@ impl CellularAutomataBuilder {
 
         self.map.tiles[exit_tile.0] = TileType::DownStairs;
         self.take_snapshot();
+
+        // Build a noise map for spawning entities
+        let mut noise = FastNoise::seeded(rng.roll_dice(1, 65536) as u64);
+        noise.set_frequency(0.08);
+        noise.set_cellular_distance_function(CellularDistanceFunction::Manhattan);
+
+        for y in 1..self.map.height-1 {
+            for x in 1..self.map.width-1 {
+                let idx = self.map.xy_idx(x, y).unwrap();
+                if self.map.tiles[idx] == TileType::Floor {
+                    let cell_value_f = noise.get_noise(x as f32, y as f32) * 10240.0;
+                    let cell_value = cell_value_f as i32;
+
+                    if self.noise_areas.contains_key(&cell_value) {
+                        self.noise_areas.get_mut(&cell_value).unwrap().push(idx);
+                    } else {
+                        self.noise_areas.insert(cell_value, vec![idx]);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -160,7 +181,9 @@ impl MapBuilder for CellularAutomataBuilder {
     }
 
     fn spawn_entities(&mut self, ecs: &mut World) {
-        // We need to rewrite this, too.
+        for area in self.noise_areas.iter() {
+            spawner::spawn_region(ecs, area.1, self.depth);
+        }
     }
 
     fn take_snapshot(&mut self) {
--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -25,10 +25,10 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = RandomNumberGenerator::new();
-    match rng.roll_dice(1, 3) {
+    match rng.roll_dice(1, 4) {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
-    // Box::new(CellularAutomataBuilder::new(new_depth))
 }
