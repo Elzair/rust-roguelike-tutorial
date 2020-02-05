cc469be5db42442bb797cab41a66faca6100ba55
Philip Woods <elzair@pop-os.localdomain>
2020-02-05 11:36:35 -0500

    Finished chapter 29

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -13,6 +13,8 @@ use cellular_automata::CellularAutomataBuilder;
 mod common;
 mod drunkard;
 use drunkard::DrunkardsWalkBuilder;
+mod maze;
+use maze::MazeBuilder;
 mod simple_map;
 use simple_map::SimpleMapBuilder;
 
@@ -27,13 +29,14 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = RandomNumberGenerator::new();
-    match rng.roll_dice(1, 7) {
+    match rng.roll_dice(1, 8) {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
         3 => Box::new(CellularAutomataBuilder::new(new_depth)),
         4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
         5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
         6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+        7 => Box::new(MazeBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
